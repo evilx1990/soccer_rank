1c975756819f6faf21bdb7a0d4605f4b51f7cf14
evilx <sergey.emelyanov@faceit-team.com>
2018-09-23 22:33:11 +0300

    v1.0

--- class/soccer.rb
+++ class/soccer.rb
@@ -1,6 +1,5 @@
 class SoccerRank
-  TEAM = /((\w+\s\w+|\w+)\s)/i
-  COUNT = /(\d+)$/
+  RGXP= /(\w+\s\w+|\w+)\s(\d+)..(\w+\s\w+|\w+)\s(\d+)/i
 
   def initialize
     @arr = []
@@ -11,9 +10,9 @@ class SoccerRank
     begin
       if File.exist?(filename)
         file = File.open(filename)
-        file.readlines.each { |line| @arr << line.chomp }
+        file.readlines.each { |line| @arr << line.match(RGXP).captures }
         file.close
-        create_table!
+        create_table
         rank_calc
         show_table
       end
@@ -25,39 +24,32 @@ class SoccerRank
 
   private
 
-  def show_table
-    @table = @table.sort_by { |_key, val| val }.reverse.to_h
-    i = 0
-    @table.each { |k, v| puts "#{i += 1}. #{k}, #{v} pts" }
-  end
-
-  def create_table!
-    unless @arr.empty? # если есть записи
-      @arr.each do |str| # итерируемся по ним
-        str.split(', ').each { |str1| @table[str1[TEAM].chomp(' ')] = 0 } # создаем таблицу команд в хеше
-      end
+  def create_table
+    @arr.each do |game|
+      @table[game[0]] = 0
+      @table[game[2]] = 0
     end
   end
 
   def rank_calc
     unless @arr.empty?
-      @arr.each do |str|
-        tmp_arr = str.split(', ')
-
-        count1 = tmp_arr[0][COUNT]
-        count2 = tmp_arr[1][COUNT]
-        team1 = tmp_arr[0][TEAM].chomp(' ')
-        team2 = tmp_arr[1][TEAM].chomp(' ')
-
-        if count1 > count2
-          @table[team1] += 3
-        elsif count1 < count2
-          @table[team2] += 3
+      @arr.each do |team|
+        if team[1] > team[3]
+          @table[team[0]] += 3
+        elsif team[1] < team[3]
+          @table[team[2]] += 3
         else
-          @table[team1] += 1
-          @table[team2] += 1
+          @table[team[0]] += 1
+          @table[team[2]] += 1
         end
       end
     end
+  end
+
+  def show_table
+    @table = @table.sort_by { |key, _val| key }.reverse.to_h # Сортируем по ключу, чтобы после сортировки по значению, команды
+    @table = @table.sort_by { |_key, val| val }.reverse.to_h # с одинаковым количеством очков были отсортированы по алвавиту
+    i = 0
+    @table.each { |k, v| puts "#{i += 1}. #{k}, #{v} pts" }
   end
 end
